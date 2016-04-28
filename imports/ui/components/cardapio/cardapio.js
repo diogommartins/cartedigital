d91ad21f7c8cf5322cab138a0cd7e8e4fee94a9d
diogommartins <diogo.martins@unirio.br>
2016-04-28 18:59:31 -0300

    Cardápio funcional, mas sem feature de pedidos

--- imports/ui/components/cardapio/cardapio.js
+++ imports/ui/components/cardapio/cardapio.js
@@ -2,3 +2,33 @@
  * Created by diogomartins on 4/25/16.
  */
 import './cardapio.html';
+
+Template.cardapio.helpers({
+    entradas: function(){
+        return Pratos.find({tipo: 'Entrada', isDisponivel: true})
+    },
+    pratosPrincipais: function(){
+        return Pratos.find({tipo: 'Prato principal', isDisponivel: true})
+    },
+    bebidas: function(){
+        return Bebidas.find({isDisponivel: true});
+    },
+    sobremesas: function(){
+        return Pratos.find({tipo: 'Sobremesa'});
+    }
+});
+
+Template.cardapio.onCreated(function(){
+    Meteor.subscribe('pratosDisponiveis');
+    Meteor.subscribe('bebidasDisponiveis');
+});
+
+Template.cardapioItem.helpers({
+    teste: function(){
+        return "";
+    }
+});
+
+Template.cardapioItem.onRendered(function(){
+    console.log('rendering', this);
+});

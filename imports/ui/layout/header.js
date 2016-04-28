f31102fd7be7512bce316e12989929cc1e3f4004
diogommartins <diogo.martins@unirio.br>
2016-04-28 17:44:12 -0300

    Slogans configuraveis

--- imports/ui/layout/header.js
+++ imports/ui/layout/header.js
@@ -5,3 +5,14 @@ import './header.html';
 
 import '../components/navigation/navigation.js';
 import '../slider/slider.js';
+
+
+Template.layoutHeader.helpers({
+    slogans: function(){
+        return RotatorySlogans.find();
+    }
+});
+
+Template.layoutHeader.onCreated(function(){
+    Meteor.subscribe('slogans');
+});
